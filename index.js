2bce992231c22ff03660de267f8781ac720e7e31
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-03-29 22:52:53 +0530

    removed / from cors origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["https://my-mdb-lemon.vercel.app/", "http://127.0.0.1:5500"],
+    origin: ["https://my-mdb-lemon.vercel.app", "http://127.0.0.1:5500"],
     optionsSuccessStatus: 200,
   })
 );
