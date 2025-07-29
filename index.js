93f5315d7378b52c34b138b44ad3888e207391bc
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-30 00:23:25 +0530

    removed a slash from cores link

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(
     origin: [
       "https://my-mdb-lemon.vercel.app",
       "http://127.0.0.1:5500",
-      "http://localhost:5173/",
+      "http://localhost:5173",
     ],
     optionsSuccessStatus: 200,
   })
