8065535bb2099baad087a7eb1f598c11a4c76ad0
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-30 00:10:11 +0530

    added a port for react.js local server - 5173

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000 || 5173;
 // console.log(portz);
 
 app.use(
