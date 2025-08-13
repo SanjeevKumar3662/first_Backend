8ba9b4845a5364a26a80f177abe8430e078253ed
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-13 20:47:49 +0530

    re-arranged parms

--- fetchCalls/search/search.js
+++ fetchCalls/search/search.js
@@ -1,11 +1,10 @@
-const getSearchResults = async ({search_type,options,query,page = 1}) =>{
-
+const getSearchResults = async (options, query_type, query, page = 1) => {
   const respose = await fetch(
-    `https://api.themoviedb.org/3/search/${search_type}?query=${query}&include_adult=false&language=en-US&page=${page}`,
+    `https://api.themoviedb.org/3/search/${query_type}?query=${query}&page=${page}`,
     options
   );
 
-  console.log(`fetching ${search_type} query:${query} page: ${page}`);
+  console.log(`fetching ${query_type} query:${query} page: ${page}`);
   const data = await respose.json();
 
   return data;
