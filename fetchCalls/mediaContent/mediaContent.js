c64d54d5281f2cd577c93a4e0297a357564f5870
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-16 22:12:09 +0530

    removed language=en and added a content_type query for images or videos

--- fetchCalls/mediaContent/mediaContent.js
+++ fetchCalls/mediaContent/mediaContent.js
@@ -1,6 +1,6 @@
-const getMediaContent = async (options, id, media_type) => {
+const getMediaContent = async (options, id, media_type,content_type) => {
   const respose = await fetch(
-    `https://api.themoviedb.org/3/${media_type}/${id}/videos?language=en-US`,
+    `https://api.themoviedb.org/3/${media_type}/${id}/${content_type}`,
     options
   );
   //id for superman 1061474
